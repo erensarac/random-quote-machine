dbea4d1d4b0bb15bfceae251d3f3d41990e80de1
erensarac <esarac@outlook.com.tr>
2024-06-28 18:02:26 +0300

    fix: add base path to vite config

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/random-quote-machine/",
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
